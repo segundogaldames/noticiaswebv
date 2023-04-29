6c245c7feeaaef64886b4ade3280b2f5eab3efe0
Segundo <segundogaldames@gmail.com>
2023-04-28 21:57:13 -0400

    Implementacion de controlador y modelo roles

--- application/Config.example.php
+++ application/Config.example.php
@@ -1,19 +1,19 @@
 <?php
 
-define('BASE_URL', 'http://localhost/axiomaframe/');
+define('BASE_URL', 'http://localhost:8080/noticiaswebv/');
 define('DEFAULT_CONTROLLER', 'index');
 define('DEFAULT_LAYOUT', 'default');
-define('APP_NAME', 'NAME_APP');
+define('APP_NAME', 'NOTICIAS_APP');
 
 define('SESSION_TIME', 10);
-define('HASH_KEY', '436f558c5734af40a92747a6b312e8ecc6ec3522c7d01565c9c191833b429182');
-define('CTRL', 'f48a2c980882da0079b098950eb796d0c1f98277ce451b22c98b2b01b5a607b2');
+define('HASH_KEY', 'ec13oGBHFWD3twF8wT4ifaVTxeEdLbv8CVOjGYL98Mo=');
+define('CTRL', 'KGUshhRZV0T9npLlGZkoL73ChhPPfh4fEfJW2S9XpZE=');
 
 define('DB_DRIVER', 'mysql');
 define('DB_HOST', 'localhost');
-define('DB_USER', 'user');
-define('DB_PASS', 'password');
-define('DB_NAME', 'base');
+define('DB_USER', 'root');
+define('DB_PASS', '1234');
+define('DB_NAME', 'noticiaswebv');
 define('DB_CHAR', 'utf8');
 define('METHODENCRIPT','AES-256-ECB');
-define('KEY','APP_AXIOMAFRAME');
+define('KEY','APP_Noticias');
---
+++ controllers/rolesController.php
@@ -0,0 +1,15 @@
+<?php
+use models\Role;
+
+class rolesController extends Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function index()
+    {
+        # code...
+    }
+}
